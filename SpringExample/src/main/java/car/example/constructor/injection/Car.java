a3077e310f5c129745b5e21e9ce8b19158be4611
komalpj2437 <komal.t.gangavarapu@gmail.com>
2025-12-17 01:31:08 -0600

    changes updated

--- SpringExample/src/main/java/car/example/constructor/injection/Car.java
+++ SpringExample/src/main/java/car/example/constructor/injection/Car.java
@@ -5,6 +5,7 @@ public class Car {
      public void DisplayDetails(){
              System.out.println("Car Details:"+specification.toString());
         }
+        //constructor injection
 
     public Car(Specification specification) {
         this.specification = specification;
